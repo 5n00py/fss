69406201b312cc03cf4ca35a42f2514e463b746a
David Schmid <david.schmid@mailbox.org>
2023-12-06 23:07:20 +0100

    feat(fzf): Add new functions to fzf_builder.sh
    - Add fzf_add_preview_window_options function
    - Add fzf_add_delimiter function
    
    These functions allow users to add preview window options and delimiters to
    the 'fzf' command. They improve the customization options for the fuzzy file
    search utility.

--- lib/fzf_builder.sh
+++ lib/fzf_builder.sh
@@ -2,11 +2,13 @@
 
 # Script: fzf_builder.sh
 # Description:
-#   Provides functions to construct a command for the 'fzf' fuzzy file search utility.
+#   Provides functions to construct a command for the 'fzf' fuzzy file search
+#   utility.
 #   Designed to be sourced in other scripts or shell sessions.
 # Example:
 #   source path/to/fzf_builder.sh
 #   fzf_add_preview 'cat {}'
+#   fzf_add_delimiter ':'
 #   fzf_build_command
 
 # Initialize the fzf command with default options
@@ -20,6 +22,27 @@ fzf_command=("fzf")
 fzf_add_preview() {
     local preview_command=$1
     fzf_command+=("--preview" "$preview_command")
+}
+
+# Function: fzf_add_preview_window_options
+# Description:
+#   Adds preview window options to the 'fzf' command.
+# Arguments:
+#   options - A string representing the preview window options (e.g.,
+#   'top:66%').
+fzf_add_preview_window_options() {
+    local options=$1
+    fzf_command+=("--preview-window" "$options")
+}
+
+# Function: fzf_add_delimiter
+# Description:
+#   Adds a delimiter option to the 'fzf' command options.
+# Arguments:
+#   delimiter - A string representing the delimiter.
+fzf_add_delimiter() {
+    local delimiter=$1
+    fzf_command+=("--delimiter" "$delimiter")
 }
 
 # Function: fzf_execute
